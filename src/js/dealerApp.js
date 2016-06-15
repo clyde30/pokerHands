0dc1d85701f1d874adac02ce4171d591da7e1176
clyde30 <ajonestx@gmail.com>
2016-06-14 22:41:36 -0600

    add(players) deal to hand objects and calc basic rank

--- src/js/dealerApp.js
+++ src/js/dealerApp.js
@@ -4,38 +4,51 @@ angular.module('dealApp', [])
     var self = this;
 
     self.dealHand = function (num) {
-      self.hand = runApp(num);
-      console.log(self.hand);
+      self.hand1 = runApp(num);
+      self.hand2 = runApp(num);
+      console.log(self.hand1);
+      console.log(self.hand2);
+
+      //finds winner. Doesn't work with push.
+      //todo -- move to function
+      if (self.hand1.rank === self.hand2.rank) {
+        console.log("push ");
+      } else {
+        if (self.hand1.rank > self.hand2.rank) {
+          console.log("Player One Wins with -->" + self.hand1.handName);
+        } else {
+          console.log("Player Two Wins with -->" + self.hand2.handName);
+        }
+      }
     };
 
     self.shuffle = function () {
       shuffle();
-      self.hand = [];
-      console.log("Shuffled")
+      self.hand1 = [];
+      self.hand2 = [];
+      console.log("Shuffled");
     };
 
     //keep dealing until hand is reached.
-    self.getHand = function () {
-      var count = 1;
-      shuffle();
-      self.hand = [];
-      self.hand = runApp(5);
-      console.log("Shuffled")
-      var getHand = self.handRankName;
-      console.log("Get this hand " + getHand);
-      console.log("Current hand = " + self.hand.handName);
-      console.log("Current hand = " + self.hand.rank);
-
-      while (getHand != self.hand.handName) {
-        count++
-        console.log(count);
-        self.count = count;
-        shuffle();
-        self.hand = [];
-        self.hand = runApp(5);
-      }
-
-
-    }
+    // self.getHand = function () {
+    //   var count = 1;
+    //   shuffle();
+    //   self.hand = [];
+    //   self.hand = runApp(5);
+    //   console.log("Shuffled")
+    //   var getHand = self.handRankName;
+    //   console.log("Get this hand " + getHand);
+    //   console.log("Current hand = " + self.hand.handName);
+    //   console.log("Current hand = " + self.hand.rank);
+    //
+    //   while (getHand != self.hand.handName) {
+    //     count++
+    //     console.log(count);
+    //     self.count = count;
+    //     shuffle();
+    //     self.hand = [];
+    //     self.hand = runApp(5);
+    //   }
+    // }
 
   }]);
