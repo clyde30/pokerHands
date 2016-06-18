3e0d19e4ae54989b6c25a1abe310a96e78275e13
clyde30 <ajonestx@gmail.com>
2016-06-18 10:43:21 -0600

    fix(players): fix error when no pairs present

--- src/js/rankPlayers.js
+++ src/js/rankPlayers.js
@@ -12,28 +12,37 @@ function rankPlayers(hand1, hand2) {
 
 function rankHandCard(hand1, hand2) {
   var winner = "Push";
+  var foundWinner = false;
 
   for(i = 0; i < hand1.pairs.length; i++){
     if(hand1.pairs[i].number !== hand2.pairs[i].number) {
       if(hand1.pairs[i].number > hand2.pairs[i].number) {
         winner = "Player 1 wins";
+        foundWinner = true;
+        break;
       } else {
         winner = "Player 2 wins";
+        foundWinner = true;
+        break;
       }
     } else {
+      foundWinner = false;
+    }
+  }
+
+  if(foundWinner === false) {
       for(i = 0; i < hand1.nopairs.length; i++){
         if(hand1.nopairs[i].number !== hand2.nopairs[i].number) {
           if(hand1.nopairs[i].number > hand2.nopairs[i].number) {
             winner = "Player 1 wins";
+            break;
           } else {
             winner = "Player 2 wins";
+            break;
           }
         }
       }
     }
+
+    console.log(winner);
   }
-
-
-  console.log(winner);
-
-}
