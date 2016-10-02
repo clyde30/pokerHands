24f27cce9f67b4c836c53acc1f19ea144728b715
Austin Jones <ajonestx@gmail.com>
2016-10-01 18:55:39 -0600

    chore: refactor winner display

--- src/js/dealerApp.js
+++ src/js/dealerApp.js
@@ -6,13 +6,9 @@ angular.module('dealApp', [])
     self.dealHand = function (num) {
       self.hand1 = runApp(num);
       self.hand2 = runApp(num);
-      console.log(self.hand1);
-      console.log(self.hand2);
 
-      rankPlayers(self.hand1, self.hand2);
-
-      //finds winner. Doesn't work with push.
-      //todo -- move to function
+      self.winner = rankPlayers(self.hand1, self.hand2);
+      console.log(self.winner);
     };
 
     self.shuffle = function () {
@@ -21,27 +17,4 @@ angular.module('dealApp', [])
       self.hand2 = [];
       console.log("Shuffled");
     };
-
-    //keep dealing until hand is reached.
-    // self.getHand = function () {
-    //   var count = 1;
-    //   shuffle();
-    //   self.hand = [];
-    //   self.hand = runApp(5);
-    //   console.log("Shuffled")
-    //   var getHand = self.handRankName;
-    //   console.log("Get this hand " + getHand);
-    //   console.log("Current hand = " + self.hand.handName);
-    //   console.log("Current hand = " + self.hand.rank);
-    //
-    //   while (getHand != self.hand.handName) {
-    //     count++
-    //     console.log(count);
-    //     self.count = count;
-    //     shuffle();
-    //     self.hand = [];
-    //     self.hand = runApp(5);
-    //   }
-    // }
-
   }]);
--- src/js/rankPlayers.js
+++ src/js/rankPlayers.js
@@ -1,13 +1,18 @@
 function rankPlayers(hand1, hand2) {
+  var _winner;
   if (hand1.rank === hand2.rank) {
-    rankHandCard(hand1, hand2)
+    _winner = rankHandCard(hand1, hand2);
 } else {
   if (hand1.rank > hand2.rank) {
-    console.log("Player One Wins with -->" + hand1.handName);
+    // console.log("Player One Wins with -->" + hand1.handName);
+    _winner = "Player One Wins with -->" + hand1.handName;
   } else {
-    console.log("Player Two Wins with -->" + hand2.handName);
+    // console.log("Player Two Wins with -->" + hand2.handName);
+    _winner = "Player Two Wins with -->" + hand2.handName;
   }
 }
+
+return _winner;
 };
 
 function rankHandCard(hand1, hand2) {
@@ -17,11 +22,11 @@ function rankHandCard(hand1, hand2) {
   for(i = 0; i < hand1.pairs.length; i++){
     if(hand1.pairs[i].number !== hand2.pairs[i].number) {
       if(hand1.pairs[i].number > hand2.pairs[i].number) {
-        winner = "Player 1 wins";
+        winner = "Player One Wins with -->" + hand1.handName;
         foundWinner = true;
         break;
       } else {
-        winner = "Player 2 wins";
+        winner = "Player Two Wins with -->" + hand2.handName;
         foundWinner = true;
         break;
       }
@@ -34,15 +39,15 @@ function rankHandCard(hand1, hand2) {
       for(i = 0; i < hand1.nopairs.length; i++){
         if(hand1.nopairs[i].number !== hand2.nopairs[i].number) {
           if(hand1.nopairs[i].number > hand2.nopairs[i].number) {
-            winner = "Player 1 wins";
+            winner = "Player One Wins with -->" + hand1.handName;
             break;
           } else {
-            winner = "Player 2 wins";
+            winner = "Player Two Wins with -->" + hand2.handName;
             break;
           }
         }
       }
     }
 
-    console.log(winner);
+    return winner;
   }
