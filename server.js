cb1bbf58d08338dee6eaac00a679ea3017647d2d
Austin Jones <ajonestx@gmail.com>
2017-05-05 15:39:15 -0600

    chore: add body parser

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(8080, function() {
     console.log("Listening on 8080");
@@ -19,6 +22,11 @@ app.get('/game', function(req, res) {
 app.get('/contact', function(req,res){
     res.sendFile(__dirname + '/contact.html');
 });
+
+app.post('/contact', function(req,res) {
+    console.log(req.body);
+    res.send("Hello " + req.body.name);
+})
 
 app.get('/rules', function(req,res){
     res.sendFile(__dirname + '/rules.html')
