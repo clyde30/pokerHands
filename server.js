95426b28024e27f53b2c843a77a08f4fc14b668c
Austin Jones <ajonestx@gmail.com>
2017-05-05 14:37:19 -0600

    feat: add routes

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.listen(8080, function() {
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/index.html");
+app.get('/', function(req,res){
+    res.sendFile(__dirname + "/home.html");
+});
+
+app.get('/game', function(req, res) {
+    res.sendFile(__dirname + "/game.html");
+});
+
+app.get('/contact', function(req,res){
+    res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/rules', function(req,res){
+    res.sendFile(__dirname + '/rules.html')
 })
